32a899155ba5cba892860bd5071378753afacd56
tangjicheng <tangjch15@gmail.com>
2023-07-25 08:48:21 +0000

    feat: add a_wrapper for test

--- a_wrapper.py
+++ a_wrapper.py
@@ -7,7 +7,9 @@ from typing import Dict
 from PIL import PngImagePlugin, Image
 import piexif
 import piexif.helper
+import gradio as gr
 
+import modules
 import modules.shared as shared
 from modules import scripts, script_callbacks, extensions
 from modules.shared import opts
@@ -49,9 +51,30 @@ def encode_pil_to_base64(image):
     return base64.b64encode(bytes_data)
 
 
+def get_default_args(script_runner):
+    #find max idx from the scripts in runner and generate a none array to init script_args
+    last_arg_index = 1
+    for script in script_runner.scripts:
+        if last_arg_index < script.args_to:
+            last_arg_index = script.args_to
+    # None everywhere except position 0 to initialize script args
+    script_args = [None]*last_arg_index
+    script_args[0] = 0
+
+    # get default values
+    with gr.Blocks(): # will throw errors calling ui function without this
+        for script in script_runner.scripts:
+            if script.ui(script.is_img2img):
+                ui_default_values = []
+                for elem in script.ui(script.is_img2img):
+                    ui_default_values.append(elem.value)
+                script_args[script.args_from:script.args_to] = ui_default_values
+
 def simple_txt2img(args: Dict):
     webui.initialize()
     script_callbacks.before_ui_callback()
+    
+    shared.demo = modules.ui.create_ui()
 
     ext_name = [iter.name for iter in extensions.extensions]
     print("[tangjicheng] extentions: ", ext_name)
@@ -65,8 +88,12 @@ def simple_txt2img(args: Dict):
     if not script_runner.scripts:
         script_runner.initialize_scripts(False)
 
-    # script_args = init_script_args(txt2imgreq, self.default_script_arg_txt2img, selectable_scripts, selectable_script_idx, script_runner)
-    script_args = []
+    default_script_args = []
+    default_script_args = [0, 'NONE:0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0\nALL:1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1\nINS:1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0\nIND:1,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0\nINALL:1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0\nMIDD:1,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0\nOUTD:1,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0\nOUTS:1,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1\nOUTALL:1,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1\nALL0.5:0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5', True, 'Disable', 'values', '0,0.25,0.5,0.75,1', 'Block ID', 'IN05-OUT05', 'none', '', '0.5,1', 'BASE,IN00,IN01,IN02,IN03,IN04,IN05,IN06,IN07,IN08,IN09,IN10,IN11,M00,OUT00,OUT01,OUT02,OUT03,OUT04,OUT05,OUT06,OUT07,OUT08,OUT09,OUT10,OUT11', 1.0, 'black', '20', False, 'ATTNDEEPON:IN05-OUT05:attn:1\n\nATTNDEEPOFF:IN05-OUT05:attn:0\n\nPROJDEEPOFF:IN05-OUT05:proj:0\n\nXYZ:::1', False, False, False, 'positive', 'comma', 0, False, False, '', 'Seed', '', None, 'Nothing', '', None, 'Nothing', '', None, True, False, False, False, 0]
+    # default_script_args = get_default_args(script_runner)
+    print(f"[tangjicheng] default_script_args: {default_script_args}")
+
+    script_args = default_script_args
 
     args.pop('script_name', None)
 
@@ -151,7 +178,7 @@ def test_txt2img():
     image = output[0]
 
     pic = Image.open(BytesIO(base64.b64decode(image)))
-    pic.save("11.jpg")
+    pic.save("14.jpg")
 
 # 1girl,  <lora:test_lora:1:0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0>
 
