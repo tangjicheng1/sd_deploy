6602bfe20b104788d75ec94661b9ca44aecacd9b
TangJicheng <130357271+TangJicheng123@users.noreply.github.com>
2023-08-01 18:48:04 +0900

    fix: change path 2

--- mlflow_wrapper.py
+++ mlflow_wrapper.py
@@ -88,7 +88,7 @@ with mlflow.start_run() as run:
         pip_requirements=["transformers","torch", "torchvision", "accelerate", "xformers","piexif", "gradio"],
         input_example=input_example,
         signature=signature,
-        code_path=["/Workspace/Repos/tang.j@ctw.inc/stable-diffusion-webui/webui_wrapper.py", "/Workspace/Repos/tang.j@ctw.inc/stable-diffusion-webui/modules/", "/Workspace/Repos/tang.j@ctw.inc/stablediffusion/ldm", "/Workspace/Repos/tang.j@ctw.inc/k-diffusion/k_diffusion"],
+        code_path=["/Workspace/Repos/satoshi.okayama@databricks.com/stable-diffusion-webui/webui_wrapper.py", "/Workspace/Repos/satoshi.okayama@databricks.com/stable-diffusion-webui/modules/"],
         await_registration_for=6000,
     )
 
