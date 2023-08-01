9a5996fb9cca16af80bb088d55b4e0add72c59e2
TangJicheng <130357271+TangJicheng123@users.noreply.github.com>
2023-08-01 18:13:55 +0900

    fix: remove assert

--- modules/paths.py
+++ modules/paths.py
@@ -16,7 +16,8 @@ for possible_sd_path in possible_sd_paths:
         sd_path = os.path.abspath(possible_sd_path)
         break
 
-assert sd_path is not None, f"Couldn't find Stable Diffusion in any of: {possible_sd_paths}"
+# assert sd_path is not None, f"Couldn't find Stable Diffusion in any of: {possible_sd_paths}"
+print(f"Couldn't find Stable Diffusion in any of: {possible_sd_paths}")
 
 path_dirs = [
     (sd_path, 'ldm', 'Stable Diffusion', []),
