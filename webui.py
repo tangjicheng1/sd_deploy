d71ee13e69c9fcab38c12d88340c472e24754dea
TangJicheng <130357271+TangJicheng123@users.noreply.github.com>
2023-08-02 18:47:22 +0900

    fix: add print modules paht

--- webui.py
+++ webui.py
@@ -239,6 +239,7 @@ def initialize():
     modelloader.cleanup_models()
     configure_opts_onchange()
 
+    print("[tangjicheng] modules.__file__: {modules.__file__}")
     modules.sd_models.setup_model()
     startup_timer.record("setup SD model")
 
