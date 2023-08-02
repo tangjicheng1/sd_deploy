b4cf27943fc254d52f268adbf0e6605daf32e7d7
TangJicheng <130357271+TangJicheng123@users.noreply.github.com>
2023-08-03 01:20:09 +0900

    fix: shared_items.py

--- modules/shared_items.py
+++ modules/shared_items.py
@@ -1,31 +1,31 @@
 
 
 def realesrgan_models_names():
-    import modules.realesrgan_model
+    from modules import realesrgan_model
     return [x.name for x in modules.realesrgan_model.get_realesrgan_models(None)]
 
 
 def postprocessing_scripts():
-    import modules.scripts
+    from modules import scripts
 
-    return modules.scripts.scripts_postproc.scripts
+    return scripts.scripts_postproc.scripts
 
 
 def sd_vae_items():
-    import modules.sd_vae
+    from modules import sd_vae
 
-    return ["Automatic", "None"] + list(modules.sd_vae.vae_dict)
+    return ["Automatic", "None"] + list(sd_vae.vae_dict)
 
 
 def refresh_vae_list():
-    import modules.sd_vae
+    from modules import sd_vae
 
-    modules.sd_vae.refresh_vae_list()
+    sd_vae.refresh_vae_list()
 
 
 def cross_attention_optimizations():
-    import modules.sd_hijack
+    from modules import sd_hijack
 
-    return ["Automatic"] + [x.title() for x in modules.sd_hijack.optimizers] + ["None"]
+    return ["Automatic"] + [x.title() for x in sd_hijack.optimizers] + ["None"]
 
 
