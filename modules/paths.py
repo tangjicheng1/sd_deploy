11637f30af254cdbd5a66b1ec7b2dbe8eb143bde
TangJicheng <130357271+TangJicheng123@users.noreply.github.com>
2023-08-03 17:51:02 +0900

    refactor: remove stable-diffusion-stability-ai

--- modules/paths.py
+++ modules/paths.py
@@ -8,18 +8,6 @@ import modules.safe  # noqa: F401
 # data_path = cmd_opts_pre.data
 sys.path.insert(0, script_path)
 
-# search for directory of stable diffusion in following places
-sd_path = None
-possible_sd_paths = [os.path.join(script_path, 'repositories/stable-diffusion-stability-ai'), '.', os.path.dirname(script_path)]
-for possible_sd_path in possible_sd_paths:
-    if os.path.exists(os.path.join(possible_sd_path, 'ldm/models/diffusion/ddpm.py')):
-        sd_path = os.path.abspath(possible_sd_path)
-        break
-
-# assert sd_path is not None, f"Couldn't find Stable Diffusion in any of: {possible_sd_paths}"
-if sd_path is None:
-    print(f"Couldn't find Stable Diffusion in any of: {possible_sd_paths}")
-
 BLIP_path = os.path.join(script_path, 'repositories/BLIP')
 
 path_dirs = [
@@ -29,7 +17,6 @@ path_dirs = [
     # (os.path.join(BLIP_path, '../k-diffusion'), 'k_diffusion/sampling.py', 'k_diffusion', ["atstart"]),
 ]
 
-print(f"[tangjicheng] sd_path: {sd_path}")
 print(f"[tangjicheng] script_path: {script_path}")
 
 paths = {}
